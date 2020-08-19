47c8ba0500e1dac59a2a155bcf366f8ba2052741
david.cruz <dfcc15@hotmail.com>
2020-08-19 14:52:38 -0500

    menu en android 2

---
+++ app/src/main/java/com/example/menu_aplicacion/ui/home/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.menu_aplicacion.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.menu_aplicacion.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_biografia, container, false)
+    }
+}
